d47cf835308fbf00f51316dcc74abf942cec2c77
Juan Manuel Cerdeira <juanmanuelcerdeira760@gmail.com>
2023-03-21 17:32:30 -0300

    new index, past & upcoming vue.js files

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const { createApp } = Vue;
+const url = "https://api-amazingevents.onrender.com/api/amazing-events";
+
+const app = createApp({
+    data(){
+        return{
+            events: [],
+            categories: []
+        }
+    },
+    created(){
+        this.fetchOnlyOne();
+    },
+    methods:{
+        async fetchOnlyOne(){
+            try{
+                let response = await fetch(url);
+                response = await response.json();
+                this.events = response.events;
+                response = response.events.map(event => event.category);
+                response = [...new Set(response)];
+                this.categories = response;
+            } catch (error){
+                console.log("Error");
+            }
+        }
+    }
+})
+
+app.mount("#app")
