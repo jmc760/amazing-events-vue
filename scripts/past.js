642cace5e610672b165d943db73160bbf2615597
Juan Manuel Cerdeira <juanmanuelcerdeira760@gmail.com>
2023-03-21 23:22:57 -0300

    add filterData function

--- scripts/past.js
+++ scripts/past.js
@@ -5,7 +5,10 @@ const app = createApp({
     data(){
         return{
             events: [],
-            categories: []
+            categories: [],
+            checks: [],
+            text: "",
+            capturedData: []
         }
     },
     created(){
@@ -17,12 +20,20 @@ const app = createApp({
                 let response = await fetch(url);
                 response = await response.json();
                 this.events = response.events;
+                this.capturedData = this.events;
                 response = response.events.map(event => event.category);
                 response = [...new Set(response)];
                 this.categories = response;
             } catch (error){
                 console.log("Error");
             }
+        },
+        filterData(){
+            this.capturedData = this.events.filter(event => {
+                return (event.name.includes(this.text) ||
+                    (event.name.toLowerCase()).includes(this.text.toLowerCase()))
+                    && (this.checks.length === 0 || this.checks.includes(event.category))
+            })
         }
     }
 })
